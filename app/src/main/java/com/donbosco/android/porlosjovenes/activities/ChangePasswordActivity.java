feb349dd89faa57edcee6164c35ada1f66944a8b
Alfredo Ariaudo <aariaudo@grupoprominente.com>
2017-09-06 15:13:49 -0300

    Add first version of ChangePasswordActivity

---
+++ app/src/main/java/com/donbosco/android/porlosjovenes/activities/ChangePasswordActivity.java
@@ -0,0 +1,16 @@
+package com.donbosco.android.porlosjovenes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.donbosco.android.porlosjovenes.R;
+
+public class ChangePasswordActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
--- app/src/main/java/com/donbosco/android/porlosjovenes/activities/RecoverPasswordActivity.java
+++ app/src/main/java/com/donbosco/android/porlosjovenes/activities/RecoverPasswordActivity.java
@@ -1,7 +1,10 @@
 package com.donbosco.android.porlosjovenes.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.donbosco.android.porlosjovenes.R;
 
@@ -12,5 +15,20 @@ public class RecoverPasswordActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recover_password);
+
+        Button btnRecoverPassword = findViewById(R.id.btn_recover_password);
+        btnRecoverPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startChangePassword();
+            }
+        });
+    }
+
+    private void startChangePassword()
+    {
+        Intent intent = new Intent(this, ChangePasswordActivity.class);
+        startActivity(intent);
     }
 }
