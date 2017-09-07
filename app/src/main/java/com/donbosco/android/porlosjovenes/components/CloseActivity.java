254a837b581cbf7a931543e792b3414ff74051e3
Alfredo Ariaudo <aariaudo@grupoprominente.com>
2017-09-07 18:17:22 -0300

    Add NavUpActivity

--- app/src/main/java/com/donbosco/android/porlosjovenes/components/CloseActivity.java
+++ app/src/main/java/com/donbosco/android/porlosjovenes/components/CloseActivity.java
@@ -2,34 +2,17 @@ package com.donbosco.android.porlosjovenes.components;
 
 import android.support.annotation.LayoutRes;
 import android.support.v4.content.ContextCompat;
-import android.view.MenuItem;
 
 import com.donbosco.android.porlosjovenes.R;
 import com.donbosco.android.porlosjovenes.util.ResourceUtil;
 
-public abstract class CloseActivity extends ToolbarActivity
+public abstract class CloseActivity extends NavUpActivity
 {
     @Override
     public void setContentView(@LayoutRes int layoutResID)
     {
         super.setContentView(layoutResID);
-        //Enable button in Toolbar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         //Set close button
         getSupportActionBar().setHomeAsUpIndicator(ResourceUtil.tintDrawable(this, R.drawable.ic_close_black_24dp, ContextCompat.getColor(this, android.R.color.white)));
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch(item.getItemId())
-        {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
