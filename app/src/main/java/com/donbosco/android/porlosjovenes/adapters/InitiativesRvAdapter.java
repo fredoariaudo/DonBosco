0cef59751935ba4d9def8fc468694e4ed8ea81d5
Alfredo Ariaudo <aariaudo@grupoprominente.com>
2017-09-25 16:48:24 -0300

    Implements initiatives dummy

--- app/src/main/java/com/donbosco/android/porlosjovenes/adapters/InitiativesRvAdapter.java
+++ app/src/main/java/com/donbosco/android/porlosjovenes/adapters/InitiativesRvAdapter.java
@@ -1,15 +1,45 @@
 package com.donbosco.android.porlosjovenes.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class InitiativesRvAdapter
+import com.donbosco.android.porlosjovenes.R;
+import com.donbosco.android.porlosjovenes.model.Initiative;
+
+public class InitiativesRvAdapter extends ArrayRvAdapter<Initiative, InitiativesRvAdapter.InitiativeViewHolder>
 {
-    private static class InitiativeViewHolder extends RecyclerView.ViewHolder
+    protected static class InitiativeViewHolder extends RecyclerView.ViewHolder
     {
+        ImageView ivIliImage;
+        TextView tvIliTitle;
+        TextView tvIliDescription;
+
         public InitiativeViewHolder(View itemView)
         {
             super(itemView);
+            ivIliImage = itemView.findViewById(R.id.iv_ili_image);
+            tvIliTitle = itemView.findViewById(R.id.tv_ili_title);
+            tvIliDescription = itemView.findViewById(R.id.tv_ili_description);
         }
+    }
+
+    @Override
+    public InitiativeViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View itemView = inflater.inflate(R.layout.initiative_rv_item, parent, false);
+        return new InitiativeViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(InitiativeViewHolder holder, int position)
+    {
+        Initiative initiative = getItems().get(position);
+        holder.tvIliTitle.setText(initiative.getTitle());
+        holder.tvIliDescription.setText(initiative.getDescription());
     }
 }
--- app/src/main/java/com/donbosco/android/porlosjovenes/fragments/InitiativesFragment.java
+++ app/src/main/java/com/donbosco/android/porlosjovenes/fragments/InitiativesFragment.java
@@ -1,8 +1,14 @@
 package com.donbosco.android.porlosjovenes.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,21 +16,108 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import com.donbosco.android.porlosjovenes.R;
+import com.donbosco.android.porlosjovenes.adapters.InitiativesRvAdapter;
+import com.donbosco.android.porlosjovenes.application.AppInfo;
+import com.donbosco.android.porlosjovenes.model.Initiative;
 
-public class InitiativesFragment extends Fragment
+import java.util.ArrayList;
+
+public class InitiativesFragment extends Fragment implements LoaderManager.LoaderCallbacks<ArrayList<Initiative>>
 {
+    private static final int INITIATIVES_LOADER_ID = 1;
+
+    private View rootView;
     private ProgressBar pbInitiatives;
+    private InitiativesRvAdapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        View rootView = inflater.inflate(R.layout.fragment_initiatives, container, false);
+        rootView = inflater.inflate(R.layout.fragment_initiatives, container, false);
 
         RecyclerView rvInitiatives = rootView.findViewById(R.id.rv_initiatives);
+        rvInitiatives.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new InitiativesRvAdapter();
+        rvInitiatives.setAdapter(adapter);
 
         pbInitiatives = rootView.findViewById(R.id.pb_initiatives);
 
+        getLoaderManager().initLoader(INITIATIVES_LOADER_ID, null, this);
+
         return rootView;
+    }
+
+    @Override
+    public Loader<ArrayList<Initiative>> onCreateLoader(int id, Bundle args)
+    {
+        return new InitiativesLoader(getContext(), pbInitiatives);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<Initiative>> loader, ArrayList<Initiative> data)
+    {
+        if(data != null)
+            adapter.setItems(data);
+
+        pbInitiatives.setVisibility(View.GONE);
+
+        if(!AppInfo.connected)
+            Snackbar.make(rootView, R.string.api_generic_error, Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<Initiative>> loader)
+    {
+    }
+
+    private static class InitiativesLoader extends AsyncTaskLoader<ArrayList<Initiative>>
+    {
+        private ProgressBar progressBar;
+        private ArrayList<Initiative> initiatives;
+
+        public InitiativesLoader(Context context, ProgressBar progressBar)
+        {
+            super(context);
+            this.progressBar = progressBar;
+        }
+
+        @Override
+        protected void onStartLoading()
+        {
+            if(initiatives != null)
+            {
+                deliverResult(initiatives);
+            }
+            else
+            {
+                progressBar.setVisibility(View.VISIBLE);
+                forceLoad();
+            }
+        }
+
+        @Override
+        public ArrayList<Initiative> loadInBackground()
+        {
+            ArrayList<Initiative> initiatives = new ArrayList<>();
+
+            for(int i=1; i<4; i++)
+            {
+                Initiative initiative = new Initiative();
+                initiative.setTitle("Iniciativa "+i);
+                initiative.setDescription("Iniciativa "+i);
+                initiatives.add(initiative);
+            }
+
+            return initiatives;
+        }
+
+        @Override
+        public void deliverResult(ArrayList<Initiative> data)
+        {
+            this.initiatives = data;
+            super.deliverResult(data);
+        }
     }
 }
