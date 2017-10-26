46c41aa199e33ef3bdca0ba5e8478e39385f07af
Pablo Poza <ppoza@grupopromiente.com>
2017-10-26 16:29:05 -0300

    Target ws to production

--- app/src/main/java/com/donbosco/android/porlosjovenes/constants/RestApiConstants.java
+++ app/src/main/java/com/donbosco/android/porlosjovenes/constants/RestApiConstants.java
@@ -3,8 +3,9 @@ package com.donbosco.android.porlosjovenes.constants;
 public class RestApiConstants
 {
     //Api base URL
-    public static final String API_BASE_URL = "http://demosweb02.grupoprominente.com/DonBoscoWebAPI-v2/api/";
+    //public static final String API_BASE_URL = "http://demosweb02.grupoprominente.com/DonBoscoWebAPI-v2/api/";
     //public static final String API_BASE_URL = "http://172.16.129.80/DonBoscoWebAPI/api/";
+    public static final String API_BASE_URL = "http://45.40.139.16/1Kporlosjoveneswebapi/api/";
 
     //Donate more URL
     public static final String DONATE_MORE_URL = "https://porlosjovenes.com/dona/";
