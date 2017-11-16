b3b152f34a6b9c4cb831105b27d786530ed21dac
Pablo Poza <ppoza@grupopromiente.com>
2017-11-16 10:52:35 -0300

    Force es_AR langauge and region

--- app/src/main/java/com/donbosco/android/porlosjovenes/application/PljApplication.java
+++ app/src/main/java/com/donbosco/android/porlosjovenes/application/PljApplication.java
@@ -2,12 +2,18 @@ package com.donbosco.android.porlosjovenes.application;
 
 import android.app.Application;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.net.ConnectivityManager;
 
 import com.donbosco.android.porlosjovenes.receivers.NetworkStateReceiver;
 
+import java.util.Locale;
+
 public class PljApplication extends Application
 {
+    private Locale locale;
+    private Configuration config;
+
     @Override
     public void onCreate()
     {
@@ -16,5 +22,23 @@ public class PljApplication extends Application
         NetworkStateReceiver networkStateReceiver = new NetworkStateReceiver();
         IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
         registerReceiver(networkStateReceiver, intentFilter);
+
+        Configuration config = getBaseContext().getResources().getConfiguration();
+        locale = new Locale("es", "AR");
+        Locale.setDefault(locale);
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) 
+    {
+        super.onConfigurationChanged(newConfig);
+        if (locale != null) {
+            newConfig.locale = locale;
+            Locale.setDefault(locale);
+            getBaseContext().getResources().updateConfiguration(newConfig, getBaseContext().getResources().getDisplayMetrics());
+        }
     }
 }
